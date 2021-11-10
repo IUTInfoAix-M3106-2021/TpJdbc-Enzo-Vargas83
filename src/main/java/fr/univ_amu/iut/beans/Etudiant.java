e64335431da1820d4cd0a29b48856be91b5a0580
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-11-10 11:03:56 +0000

    ex2

--- src/main/java/fr/univ_amu/iut/beans/Etudiant.java
+++ src/main/java/fr/univ_amu/iut/beans/Etudiant.java
@@ -1,6 +1,7 @@
 package fr.univ_amu.iut.beans;
+import java.io.Serializable;
 
-public class Etudiant {
+public class Etudiant implements Serializable {
     private int numEt;
     private String nomEt;
     private String prenomEt;
@@ -78,4 +79,58 @@ public class Etudiant {
                 + "annee=" + annee + ", groupe=" + groupe + ", "
                 + "]";
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + annee;
+        result = prime * result + ((cpEt == null) ? 0 : cpEt.hashCode());
+        result = prime * result + groupe;
+        result = prime * result + ((nomEt == null) ? 0 : nomEt.hashCode());
+        result = prime * result + numEt;
+        result = prime * result + ((prenomEt == null) ? 0 : prenomEt.hashCode());
+        result = prime * result + ((villeEt == null) ? 0 : villeEt.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Etudiant other = (Etudiant) obj;
+        if (annee != other.annee)
+            return false;
+        if (cpEt == null) {
+            if (other.cpEt != null)
+                return false;
+        } else if (!cpEt.equals(other.cpEt))
+            return false;
+        if (groupe != other.groupe)
+            return false;
+        if (nomEt == null) {
+            if (other.nomEt != null)
+                return false;
+        } else if (!nomEt.equals(other.nomEt))
+            return false;
+        if (numEt != other.numEt)
+            return false;
+        if (prenomEt == null) {
+            if (other.prenomEt != null)
+                return false;
+        } else if (!prenomEt.equals(other.prenomEt))
+            return false;
+        if (villeEt == null) {
+            if (other.villeEt != null)
+                return false;
+        } else if (!villeEt.equals(other.villeEt))
+            return false;
+        return true;
+    }
+
+    
 }
