ae5deaa7735059387149c80c875259e5de5115a0
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-11-28 20:36:11 +0000

    ex6 (pas sur du resultat attendus)

--- src/main/java/fr/univ_amu/iut/ExempleDaoEtudiant.java
+++ src/main/java/fr/univ_amu/iut/ExempleDaoEtudiant.java
@@ -2,8 +2,8 @@ package fr.univ_amu.iut;
 
 // Importer les classes jdbc
 import java.sql.*;
+import java.util.List;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.*;
@@ -15,50 +15,19 @@ import fr.univ_amu.iut.DAO.DAOEtudiant;
 import fr.univ_amu.iut.DAO.JDBC.DAOEtudiantJDBC;
 
 public class ExempleDaoEtudiant {    
-    // La requete de test
-    private static final String reqEtudiantsAixois =
-            "SELECT NUM_ET, NOM_ET, PRENOM_ET, CP_ET, VILLE_ET, ANNEE, GROUPE  " +
-                    "FROM ETUDIANT " +
-                    "WHERE VILLE_ET = 'AIX-EN-PROVENCE'";
-
     public static void main(String[] args) throws SQLException {
         // Connexion a la base de données
         System.out.println("Connexion");
-        try (Connection conn = ConnexionUnique.getInstance().getConnection()) {
-            System.out.println("Connecte\n");
-            
-            // Creation d'une instruction SQL
-            Statement statement = conn.createStatement();
-            
-            // Execution de la requete
-            System.out.println("Execution de la requete : " + reqEtudiantsAixois);
-            ResultSet resultSet = statement.executeQuery(reqEtudiantsAixois);
-            
+        try {
             DAOEtudiant Dao = new DAOEtudiantJDBC();
-            // Affichage du resultat
-            while (resultSet.next()) {
+            List<Etudiant> listET = new ArrayList<Etudiant>();
+            listET = Dao.findAll();
 
-                Etudiant etu = new Etudiant();
-                etu.setNumEt(resultSet.getInt("NUM_ET"));
-                etu.setNomEt(resultSet.getString("NOM_ET"));
-                etu.setPrenomEt(resultSet.getString("PRENOM_ET"));
-                etu.setCpEt(resultSet.getString("CP_ET"));
-                etu.setVilleEt(resultSet.getString("VILLE_ET"));
-                etu.setAnnee(resultSet.getInt("ANNEE"));
-                etu.setGroupe(resultSet.getInt("GROUPE"));
-                
-                System.out.println(etu.toString());
-                etu = Dao.insert(etu);
-                boolean updateOK = Dao.update(etu);
-    
-            }
-
-            // Fermeture de l'instruction (liberation des ressources)
-            statement.close();
-            System.out.println("\nOk.\n");
-        } catch (SQLException e) {
+            listET.forEach(etudiant -> System.out.println(etudiant.toString()));
+  
+            } catch (Exception e) {
             e.printStackTrace();// Arggg!!!
             System.out.println(e.getMessage() + "\n");
         }
     }
-    }
+}
